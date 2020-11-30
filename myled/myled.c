2403bcde62fc70cd202e4a614739980a8d3361a5
Kaito Yamazaki <s19c1118hc@s.chibakoudai.jp>
2020-11-30 07:09:37 +0000

    メジャー番号の確認

--- myled/myled.c
+++ myled/myled.c
@@ -11,21 +11,21 @@ static dev_t dev;
 static int __init init_mod(void)
 {
 	int retval;
-	retval = alloc_chrdev_region(&dev,0,1,"myled");
+	retval =  alloc_chrdev_region(&dev, 0, 1, "myled");
 	if(retval < 0){
 		printk(KERN_ERR "alloc_chrdev_region failed.\n");
 		return retval;
 	}
-	printk(KERN_INFO "%s is loaded. major;%d\n", __FILE__,MAJOR(dev));
+
+	printk(KERN_INFO "%s is loaded. major:%d\n",__FILE__,MAJOR(dev));
 	return 0;
 }
 
 static void __exit cleanup_mod(void)
 {
-	unregister_chrdev_region(dev,1);
-	printk(KERN_INFO "%s is unloaded. major:%d\n", __FILE__,MAJOR(dev));
+	unregister_chrdev_region(dev, 1);
+	printk(KERN_INFO "%s is unloaded. major:%d\n",__FILE__,MAJOR(dev));
 }
 
 module_init(init_mod);
 module_exit(cleanup_mod);
-
