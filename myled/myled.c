000e4d60f601026894f65300f188f4ff3a5bc1fd
Kaito Yamazaki <s19c1118hc@s.chibakoudai.jp>
2020-11-30 08:51:57 +0000

    クラスも増やした

--- myled/myled.c
+++ myled/myled.c
@@ -1,6 +1,7 @@
 #include <linux/module.h>
 #include <linux/fs.h>
 #include <linux/cdev.h>
+#include <linux/device.h>
 
 MODULE_AUTHOR("Kaito Yamazaki");
 MODULE_DESCRIPTION("driver for LED control");
@@ -9,6 +10,7 @@ MODULE_VERSION("0.0.1");
 
 static dev_t dev;
 static struct cdev cdv;
+static struct class *cls = NULL;
 
 static ssize_t led_write(struct file* filp, const char* buf, size_t count, loff_t* pos)
 {
@@ -38,12 +40,19 @@ static int __init init_mod(void)
 		return retval;
 	}
 
+	cls = class_create(THIS_MODULE, "myled");
+	if(IS_ERR(cls)){
+		printk(KERN_ERR "class_create failed.");
+			return PTR_ERR(cls);
+	}
+
 	return 0;
 }
 
 static void __exit cleanup_mod(void)
 {
 	cdev_del(&cdv);
+	class_destroy(cls);
 	unregister_chrdev_region(dev, 1);
 	printk(KERN_INFO "%s is unloaded. major:%d\n",__FILE__,MAJOR(dev));
 }
